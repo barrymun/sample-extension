7685d493f34af6d0885590089f96ed28ea8d7622
Neil Murphy <barrymun@tcd.ie>
2023-10-12 22:29:05 +0100

    fade in effect

--- extension/src/utils/constants.ts
+++ extension/src/utils/constants.ts
@@ -1,8 +1,11 @@
 export const panelWidth: string = "500px";
 
-export const panelId: string = "extension-panel";
+export const panelId: string = "sapphire-panel";
 export const panelClosedStyle: string = `
     width: 0;
+    opacity: 0;
+    transform: translateX(100%);
+    transition: opacity 0.3s, transform 0.3s;
 `;
 export const panelOpenStyle: string = `
     position: fixed;
@@ -13,6 +16,9 @@ export const panelOpenStyle: string = `
     width: ${panelWidth};
     box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1), 0px 1px 3px rgba(0, 0, 0, 0.1);
     border-radius: 4px;
-    z-index: 10000;
+    z-index: 2147483647;
     background-color: #fff;
+    opacity: 1;
+    transform: translateX(0);
+    transition: opacity 0.3s, transform 0.3s;
 `;
