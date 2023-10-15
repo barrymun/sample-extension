a1ff6a84a954f5bfd1946dfbf28b64dc71212fd2
Neil Murphy <barrymun@tcd.ie>
2023-10-15 21:36:13 +0100

    more graceful fade in

--- extension/src/utils/constants.ts
+++ extension/src/utils/constants.ts
@@ -4,8 +4,8 @@ export const panelId: string = "sapphire-panel";
 export const panelClosedStyle: string = `
   width: 0;
   opacity: 0;
-  transform: translateX(100%);
-  transition: opacity 0.3s, transform 0.3s;
+  // transform: translateX(100%);
+  transition: opacity 0.6s;
 `;
 export const panelOpenStyle: string = `
   position: fixed;
@@ -17,10 +17,11 @@ export const panelOpenStyle: string = `
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1), 0px 1px 3px rgba(0, 0, 0, 0.1);
   border-radius: 4px;
   z-index: 2147483647;
+  // background-color: transparent;
   background-color: #fff;
   opacity: 1;
-  transform: translateX(0);
-  transition: opacity 0.3s, transform 0.3s;
+  // transform: translateX(0);
+  transition: opacity 0.6s;
 `;
 
 export const panelFrameId: string = "sapphire-panel-frame";
