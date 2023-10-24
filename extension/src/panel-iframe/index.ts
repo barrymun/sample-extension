d211403a44d28c192ef6f68e841ea4396d6f679c
Neil Murphy <barrymun@tcd.ie>
2023-10-24 15:37:59 +0100

    working message dance (extension, remote-extension and app)

--- extension/src/panel-iframe/index.ts
+++ extension/src/panel-iframe/index.ts
@@ -7,7 +7,17 @@ const onLoad = () => {
 };
 
 const onMessage = (event: MessageEvent<Message>) => {
-  console.log(event.data);
+  const iframe = document.getElementById("sapphire-panel-frame-remote") as HTMLIFrameElement;
+  switch (event.data.type) {
+    case MessageType.GET_PAGE_DATA:
+      window.parent.postMessage(event.data, "*");
+      break;
+    case MessageType.SEND_PAGE_DATA:
+      iframe.contentWindow?.postMessage(event.data, "*");
+      break;
+    default:
+      break;
+  }
 };
 
 window.addEventListener("load", onLoad);
