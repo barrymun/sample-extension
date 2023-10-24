7365d88ee9068967c2f48a36bd32349a131c0186
Neil Murphy <barrymun@tcd.ie>
2023-10-24 15:42:46 +0100

    reformat

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,26 +5,24 @@ import { useEffect } from "react";
 // import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("page");
-
-  const onLoad = () => {
-    const message = { type: "GET_PAGE_DATA" }; // TODO: make a "common" package to share types
-    window.parent.postMessage(message, "*");
-  };
-
   const onMessage = (event: MessageEvent) => {
     console.log("message received", event.data);
   };
-
-  useEffect(() => {
-    onLoad();
-  }, []);
 
   useEffect(() => {
     window.addEventListener("message", onMessage);
     return () => {
       window.removeEventListener("message", onMessage);
     };
+  }, []);
+
+  const onLoad = () => {
+    const message = { type: "GET_PAGE_DATA" }; // TODO: make a "common" package to share types
+    window.parent.postMessage(message, "*");
+  };
+
+  useEffect(() => {
+    onLoad();
   }, []);
 
   return <div>HERE</div>;
